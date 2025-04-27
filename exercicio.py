e9a717c6142b9b8fa2abcd989a5c024e35dd4a31
Dayane Araújo <122062822+dayanearauj0@users.noreply.github.com>
2025-04-26 21:58:48 -0300

    Exercício entrada e saída de dados

---
+++ exercicio.py
@@ -0,0 +1,12 @@
+# entradas/saída de dados: nome, idade e peso
+
+# entrada de dados 
+
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+peso = float(input("digite seu peso: "))
+
+# saída de dados
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"peso: {peso}")
